f128ee97d7779262546887cd0e2527b9a9c77c54
Vinay Pal Singh <739349@student.tor.loc>
2019-05-16 16:40:52 -0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+<script>
+let itemPrice1 = 20;
+let itemPrice2 = 30;
+let sum = itemPrice1 + itemPrice2;
+let firstname = "joe";
+let lastname = "smith";
+alert("Hello" + firstname + " " + lastname);
+</script>
